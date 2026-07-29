0513d6712d597f14f3718aca87c8f961f376d87d
fliper-sys <lovebarry839@gmail.com>
2026-07-29 23:47:43 +0100

    backend optimization abd cleanup

--- lib/data/repositories/industry_specific/salon_repository.dart
+++
@@ -1,6 +0,0 @@
-abstract class SalonRepository {
-  Future<List<Map<String, dynamic>>> fetchAppointments(String businessId);
-  Future<void> bookAppointment(Map<String, dynamic> appointment);
-  Future<List<Map<String, dynamic>>> fetchStylists(String businessId);
-}
-
